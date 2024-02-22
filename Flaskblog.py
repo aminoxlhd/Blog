6ae9d013bffa430c339f8e9ebeee8568ebb7e4be
aminoxlhd <aminoxlhd@gmail.com>
2024-02-22 21:34:46 +0100

    first day

---
+++ Flaskblog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+
+posts = [
+    {
+        'autor': 'Leonel Messi',
+        'title': 'Win world Cup',
+        'content': 'Fist Post Content',
+        'date_posted': 'December 18, 2022'
+    },
+    {
+        'autor': 'Laheroud amine',
+        'title': 'Complet Alx SE',
+        'content': 'Second Post Content',
+        'date_posted': 'July 22, 2024'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+if __name__ == "__main__":
+    app.run(debug=True)
