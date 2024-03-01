34dc01253048adcd23ac20be1e3a193195bbfc1e
aminoxlhd <aminoxlhd@gmail.com>
2024-03-01 03:13:09 +0100

    Add Blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '5d26fc3a897c6563c48af2f3eca6c201'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
